98bb329f51f032f456e0782ca157a6d235736a97
Punit Bathija <punit644@gmail.com>
2023-02-22 13:51:05 +0530

    Added a new directory to check out the navigation throughout

---
+++ basics/basics/src/app/projects/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Head from "next/head";
+import image from "next/image";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+    </>
+  );
+}
