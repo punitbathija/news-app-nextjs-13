41c906385e1788c7dcd9686aaad91343ca7771a1
Punit Bathija <punit644@gmail.com>
2023-02-23 15:18:31 +0530

    Initialized the nextjs app with tailwind css template

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -5,10 +5,5 @@ import styles from "./page.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className={styles.center}>
-      <h1>Next.Js!</h1>
-      <p>Figuring out how to use the Next.js 13</p>
-    </div>
-  );
+  return <h1>Hello World!</h1>;
 }
